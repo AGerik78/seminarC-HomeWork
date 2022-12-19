c81fb70b699f4a1e8ef1b64d358d7e962a6e63bb
Alexey <gercoo@mail.ru>
2022-12-19 20:05:16 +0300

    new file seminar001

--- seminar001_practice/Program.cs
+++ seminar001_practice/Program.cs
@@ -1,2 +1,88 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = int.Parse(System.Console.ReadLine()!);
+
+int i = -n;
+
+while (i <= n)
+{
+    System.Console.WriteLine(i + ", ");
+    i++;
+}
+
+/*int a, b;
+
+Console.WriteLine("Введите первое число");
+
+a = int.Parse(System.Console.ReadLine()!);
+
+System.Console.WriteLine("Введите второе число являющееся квадратом первого");
+
+b = int.Parse(System.Console.ReadLine()!);
+
+if (a * a == b)
+{
+    System.Console.WriteLine($"Да, {b} это {a} в квадрате");
+
+}
+else
+{
+    System.Console.WriteLine($"Нет, {b} не является квадратом {a}");
+}
+*/
+/*int number = 1234;
+
+System.Console.WriteLine(number + number);
+
+string someString = "1234";
+
+System.Console.WriteLine(someString + someString);
+*/
+
+// 1234 + 1234
+
+// 1234 / 1234
+
+// 1234 % 100 == 34
+
+/* System.Console.WriteLine("Введите число: ");
+
+number = int.Parse(Console.ReadLine()!);
+
+int squareOfNumber = number * number;
+
+// System.Console.WriteLine("Квадрат числа " + number +  " равен " + squareOfNumber);
+
+System.Console.WriteLine($"Квадрат числа {number} = {squareOfNumber}");  // $ - форматирует строку!!!
+*/
+
+
+/*number = int.MaxValue;
+System.Console.WriteLine("Max value");
+System.Console.WriteLine(number);
+
+number = number + 1;
+
+System.Console.WriteLine("Max value + 1");
+System.Console.WriteLine(number);
+
+//System.Console.WriteLine(int.MaxValue);
+//System.Console.WriteLine(int.MinValue);
+*/
+
+
+
+
+
+// Convert.ToInt32(Console.ReadLine()!);
+
+/* string input;
+
+input = System.Console.ReadLine();
+
+System.Console.WriteLine(input);
+System.Console.WriteLine(input);
+
+*/
+
+// number = int.Parse(Console.ReadLine());
+
+//Console.WriteLine(number);
