79e0ca72bfae0947e4013269731f0d78e07d4193
Alexey <gercoo@mail.ru>
2022-12-20 13:58:40 +0300

    homeWork 2,4,6,8

---
+++ HomeWork_1_004/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+int a, b, c;
+
+Console.WriteLine("Введите первое натуральное целое число");
+
+a = int.Parse(System.Console.ReadLine()!);
+
+System.Console.WriteLine("Введите второе натуральное целое число ");
+
+b = int.Parse(System.Console.ReadLine()!);
+
+System.Console.WriteLine("Введите третье натуральное целое число ");
+
+c = int.Parse(System.Console.ReadLine()!);
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+System.Console.WriteLine($"Максимальное число {max}");
