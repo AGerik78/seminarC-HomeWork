41966f0c52f14392ec44d82cb21cd391833ea138
Alexey <gercoo@mail.ru>
2022-12-20 16:50:36 +0300

    homeWork_2_010 function

--- HomeWork_2_010/Program.cs
+++ HomeWork_2_010/Program.cs
@@ -6,9 +6,9 @@
 918 -> 1
 */
 
-int a;
+/*int a;
 
-Console.WriteLine("Введите натуральное целое число");
+Console.WriteLine("Введите натуральное целое трехзначное число");
 
 a = int.Parse(System.Console.ReadLine()!);
 
@@ -16,3 +16,17 @@ int a2 = a / 10 % 10;
  
 
 System.Console.WriteLine(a2);
+*/
+
+int GetTwoDigit(int number)
+{
+    return number = number / 10 % 10;
+}
+
+
+Console.WriteLine("Введите натуральное целое трехзначное число");
+
+int number = int.Parse(System.Console.ReadLine()!);
+
+
+System.Console.WriteLine($"Вторая цифра числа {number} - {GetTwoDigit(number)}");
