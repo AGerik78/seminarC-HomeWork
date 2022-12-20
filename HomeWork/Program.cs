149e185b7a700195919a8083e67b9c675cf33957
Alexey <gercoo@mail.ru>
2022-12-20 13:57:38 +0300

    homeWork 2,4,6,8

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+int a, b;
+
+Console.WriteLine("Введите первое число");
+
+a = int.Parse(System.Console.ReadLine()!);
+
+System.Console.WriteLine("Введите второе число ");
+
+b = int.Parse(System.Console.ReadLine()!);
+
+if ( a > b)
+{
+    System.Console.WriteLine($"Болшее число {a}, а меньшее число {b}");
+}
+else
+{
+    System.Console.WriteLine($"Большее число {b}, а меньшее число {a}");
+}
---
+++ HomeWork_1_008/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int N;
+
+Console.WriteLine("Введите натуральное целое число больше 1");
+
+N = int.Parse(System.Console.ReadLine()!);
+
+int count = 1;
+
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        System.Console.WriteLine(count);
+    }
+    count++;
+}
+
+// Хотел сделать чтобы при вводе 0 и 1 запрашивало на повторный ввод, не получилось((...
+
+/*if (N = 0)
+{
+    System.Console.WriteLine("Вы ввели слишком маленькое число");
+    N = int.Parse(System.Console.ReadLine()!);
+}
+else
+{
+    while (count <= N)
+    {
+        if (count % 2 == 0)
+        {
+            System.Console.WriteLine(count);
+        }
+        count++;
+    }
+
+}
+*/
