bb0325e0ddf5b79c7fbe1f2c5006aa1343e86612
Alexey <gercoo@mail.ru>
2022-12-28 11:25:02 +0300

    homeWork_3_023 решение

---
+++ HomeWork_3_023/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+int TableOfCubes(int n)
+{
+    int i = 1;
+    while (i <= n)
+    {
+        System.Console.Write(i * i * i + ", ");
+        i++;
+    }
+    return n;
+}
+
+System.Console.Write("Введите натуральное число: ");
+int n = int.Parse(System.Console.ReadLine()!);
+
+int cubes = TableOfCubes(n);
+
+
+
